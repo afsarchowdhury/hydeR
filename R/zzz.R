fbabb627cc8d5d4982b9b0bdfaae18ae45109599
afsarchowdhury <a.chowdhury@hydehighschool.uk>
2023-06-01 23:37:46 +0100

    Update startup message for citation.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,13 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0(
-    "You will need a valid API key to access the functions in this pacakge.",
-    "\nAsk your school administrator to generate one for you.",
-    "\nLog errors and bugs at https://github.com/afsarchowdhury/hydeR/issues."
-  ))
+  packageStartupMessage(
+    paste0(
+      "You will need a valid API key to access the functions in this pacakge.",
+      "\nAsk your school administrator to generate one for you.",
+      "\nLog errors and bugs at https://github.com/afsarchowdhury/hydeR/issues."
+    )
+  )
+
+  packageStartupMessage(
+    message(cat(crayon::cyan("Type citation('hydeR') to cite hydeR in a publication.")))
+  )
 }
